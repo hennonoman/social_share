12cc1c08f94e9e272210f94f2299104aee5e527b
Artem Sivtsev <artemsivcev@gmail.com>
2021-08-15 10:37:19 +0200

    4

--- ios/Classes/SocialSharePlugin.m
+++ ios/Classes/SocialSharePlugin.m
@@ -85,14 +85,14 @@
     } else if ([@"shareInstagramFeed" isEqualToString:call.method]) {
     NSString *stickerImage = call.arguments[@"imagePath"];
     NSURL *urlScheme = [NSURL URLWithString:@"instagram://app"];
-    NSLog(@"file://%@", stickerImage);
+    NSLog(@"path = %@", stickerImage);
    // NSLog(@[NSString stringWithFormat:@"%@.igo", stickerImage]);
      if ([[UIApplication sharedApplication] canOpenURL:urlScheme]) {
         NSLog(@"1");
         UIViewController* controller = [UIApplication sharedApplication].delegate.window.rootViewController;
         NSLog(@"2");
         //[[NSFileManager defaultManager] moveItemAtPath:stickerImage toPath:[NSString stringWithFormat:@"%@.igo", stickerImage] error:&error];
-        NSURL *path = [NSURL URLWithString:stickerImage];
+        NSURL *path = [NSURL URLWithString:[NSString stringWithFormat:@"file:/%@", stickerImage]];
         NSLog(@"2,5");
         UIDocumentInteractionController* _dic = [UIDocumentInteractionController interactionControllerWithURL:path];
         NSLog(@"3");
