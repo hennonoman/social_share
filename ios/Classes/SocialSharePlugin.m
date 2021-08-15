46815609a844e73372ab44e09d62e48ea4eef801
Artem Sivtsev <artemsivcev@gmail.com>
2021-08-15 12:52:36 +0200

    29

--- ios/Classes/SocialSharePlugin.m
+++ ios/Classes/SocialSharePlugin.m
@@ -122,7 +122,7 @@
          NSLog(@"3,1");
         _dic.UTI = @"com.instagram.exclusivegram";
          NSLog(@"3,2");
-         _dic = [UIDocumentInteractionController setupControllerWithURL:imageURL usingDelegate:controller];
+         _dic = [self setupControllerWithURL:imageURL usingDelegate:self];
           NSLog(@"3,3");
          _dic=[UIDocumentInteractionController interactionControllerWithURL:imageURL];
           NSLog(@"3,4");
